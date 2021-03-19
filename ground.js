ea1df37496092bfa0caea4671383fafbb3cccc63
Pranjal-2006 <74821791+Pranjal-2006@users.noreply.github.com>
2021-03-19 12:42:53 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,31 @@
+class ground
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var gPos=this.body.position;		
+
+			push()
+			translate(gPos.x, gPos.y);
+			rectMode(CENTER)
+			//strokeWeight(5);
+            fill("pink")
+        			rect(0,0,this.w, this.h);
+			pop()
+			
+	}
+
+}
